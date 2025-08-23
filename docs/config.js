c768ede6a9e3bd31067ddb43f73c81b978668eb4
Rangga Fermata <41527863+ranggafermata@users.noreply.github.com>
2025-08-23 10:16:47 +0700

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -2,4 +2,5 @@
 
 const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
   ? 'http://localhost:8080'  // URL for local development
-  : 'https://your-backend-name.onrender.com'; // URL for production
+
+  : 'https://huggingface.co/spaces/ranggafermata/Bangorinas'; // URL for production
