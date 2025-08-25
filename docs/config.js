d1227e3ee1263faca6346548e1bbb68a985b090e
Rangga Fermata <41527863+ranggafermata@users.noreply.github.com>
2025-08-25 10:41:43 +0700

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -1,7 +1,12 @@
-// config.js
+// docs/config.js
 
-const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-  ? 'http://localhost:8080'  // URL for local development
+// URL for your TEXT model service (the one with Llama/Endeavor)
+const TEXT_API_URL = window.location.hostname === 'localhost'
+  ? 'http://localhost:8080' // For local testing
+  : 'https://bangorinas-bangorinas-backend-text.hf.space'; // 
 
-  : 'https://ranggafermata-bangorinas.hf.space'; // URL for production
+// URL for your VISION model service (the one with BLIP)
+const VISION_API_URL = window.location.hostname === 'localhost'
+  ? 'http://localhost:8081' // For local testing
+  : 'https://ranggafermata-bangorinas.hf.space'; // 
 
